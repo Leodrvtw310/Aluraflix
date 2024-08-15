fe4bebede879ecda59ee4fb8b0faee3573f1bc28
Leodrvtw310 <00001228758190sp@al.educacao.sp.gov.br>
2024-08-14 21:46:23 -0300

    Atualizar o script.js

--- script.js
+++ script.js
@@ -1,33 +1,11 @@
-body {
-    background-color: #1D4221;
-    color: white;
-    font-family: "Bai Jamjuree", sans-serif;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    height: 700px;
-    margin: 0;
-}
+const avanca = document.querySelectorAll('.btn-proximo');
 
-button {
-    background-color: #64A712;
-    color: white;
-    font-family: "Bai Jamjuree", sans-serif;
-}
+avanca.forEach(button => {
+    button.addEventListener('click', function(){
+        const atual = document.querySelector('.ativo');
+        const proximoPasso = 'passo-' + this.getAttribute('data-proximo');
 
-.passo {
-    display: none;
-}
-
-.passo.ativo {
-    display:block;
-}
-
-main {
-    text-align: center;
-    max-width: 90%;
-}
-
-img {
-    max-width: 90%;
-}
+        atual.classList.remove('ativo');
+        document.getElementById(proximoPasso).classList.add('ativo');
+    })
+})
